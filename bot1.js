61bef9378681aff9cd654cae542535691ea1542d
MRDGH2821 <mihirrabade@gmail.com>
2021-12-03 08:54:30 +0530

    Updated to v1.2.0

--- bot1.js
+++ bot1.js
@@ -1,33 +1,36 @@
 const { Client } = require("discord.js"); // V13 Now :]
 const { token } = require("./config.json");
 const client = new Client({
-  intents: ["GUILDS", "GUILD_BANS", "GUILD_MESSAGE_TYPING"]
-
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_BANS", "GUILD_MESSAGE_TYPING"]
 });
 
-client.once("ready", () => {
-  console.log("Bot is online! Use !unban-all to unban all users.");
+client.on("ready", () => {
+  console.log("Bot is online! Use !unbanall to unban all users.");
 });
 
 client.on("messageCreate", message => {
   switch (message.content.toLowerCase()) {
     case "!unbanall":
       if (message.member.permissions.has("BAN_MEMBERS")) {
+        message.reply({
+          content: "Preparing to mass Unban"
+        });
         message.guild.bans
           .fetch()
           .then(bans => {
+            console.log(bans[0]);
             if (bans.size == 0) {
-              message.reply({ content: "There are no banned users." });
               throw "No members to unban.";
+            } else {
+              bans.forEach(ban => {
+                message.guild.members.unban(ban.user.id);
+              });
             }
-            bans.forEach(ban => {
-              message.guild.members.unban(ban.user.id);
-            });
           })
-          .then(() => console.log("Users are being unbanned."))
-          .catch(e => console.log(e))
-          .then(message.reply({ content: "Mass-Unban successful" }));
+          .catch(e => message.reply({ content: e }))
+          .finally(message.reply({ content: "Process finished" }));
       } else {
+        message.reply("You do not have enough permissions for this command");
         console.log("You do not have enough permissions for this command.");
       }
       break;
